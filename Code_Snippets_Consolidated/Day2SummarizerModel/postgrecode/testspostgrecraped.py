4ec9ace1b4831dca0c5953bb63c2daa3fa83c795
Mark Anjelo Perez <111416671+jelopogi123456@users.noreply.github.com>
2023-04-26 17:20:26 +0800

    Add files via upload

---
+++ Code_Snippets_Consolidated/Day2SummarizerModel/postgrecode/testspostgrecraped.py
@@ -0,0 +1,21 @@
+import psycopg2
+import pandas as pd
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    dbname="articles",
+    user="postgres",
+    password="interntest123",
+    host="localhost",
+    port="5432"
+)
+
+# Query the 'scraped_articles' table
+query = "SELECT * FROM scraped_articles;"
+df = pd.read_sql_query(query, conn)
+
+# Close the connection
+conn.close()
+
+# Print the results
+print(df)
